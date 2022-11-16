520bbca92620c3b60cac0b4d6aa2d43867445512
{Remal} <{f201103@cfd.nu.edu.pk}>
2022-11-16 13:37:28 +0500

    Edit Transfer layer

--- src/transferObject/TransferContent.java
+++ src/transferObject/TransferContent.java
@@ -1,5 +1,37 @@
 package transferObject;
 
+import java.util.ArrayList;
+
 public class TransferContent {
 
+	private String title;
+	private String author;
+	private String content;
+	private ArrayList<String> words = new ArrayList<String>();
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public ArrayList<String> getWords() {
+		return words;
+	}
+	public void setWords(ArrayList<String> words) {
+		this.words = words;
+	}
+	
+	
 }
